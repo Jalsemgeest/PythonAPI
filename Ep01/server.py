ec8e5d5d030ed281fb55ec166db9eb2647fe2b94
Jake Alsemgeest <alsemgeester@gmail.com>
2022-12-16 11:39:52 -0500

    Fixed the server.py for Ep01

--- Ep01/server.py
+++ Ep01/server.py
@@ -2,14 +2,14 @@
 Run with `flask --app server --debug run`
 '''
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 # Get request - it will return just what you tell it to
 @app.route("/")
 def index():
-	return "Running!"
+	return "Subscribe to JakeEh!"
 
 # Get request - it will return based on the input of the name parameter in the URL
 @app.route("/hello/<name>")
