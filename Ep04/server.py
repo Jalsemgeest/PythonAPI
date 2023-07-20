4872ec8b477986a1b52314ebfbf7dbd2bd1b20f6
JakeEh <alsemgeester@gmail.com>
2023-07-19 20:34:57 -0400

    Changes for video

--- Ep04/server.py
+++ Ep04/server.py
@@ -1,36 +1,21 @@
-'''
-Run with `flask --app server --debug run`
-'''
 
-from flask import Flask, request, render_template
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
-# Get request - it will return just what you tell it to
-@app.route("/")
+messages = [ "Subscribe", "Like", "Comment", "Eat a cookie" ]
+
+@app.route('/')
 def index():
-	return "Running!"
+    message = messages[random.randint(0, 3)]
+    
+    return render_template('index.html', message=message)
 
-# Get request - it will return based on the input of the name parameter in the URL
-@app.route("/hello/<name>")
-def helloWorld(name=None):
-	return "Hello, " + name + "!"
+@app.route('/about')
+def about():
+  
+    return render_template('about.html')
 
-# Here you can use query parameters. The first must start with ?key=value, then &key=value after that.
-# Spaces cannot be used. If you want to add a space you need to do %20 as it's the value for a space!
-@app.route("/login", methods=["GET", "POST"])
-def query():
-	args = request.args
-	print(request.method)
-	if request.method == "GET":
-		return "Get: " + args.get("name", default="Sami") + " is " + args.get("age", default="4") + " years old."
-	else:
-		print(request.form)
-		print(request.data)
-		return args
-
-@app.route("/hi/<name>")
-def hi(name=None):
-	return render_template("hi.html", name=name)
-
-print("Running")
+if __name__ == '__main__':
+    app.run(debug=True)
